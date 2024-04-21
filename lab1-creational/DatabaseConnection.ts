ae9c16ec52a05b6eccc1abc312272d4d42f33ca2
Iana Gamurar <iana.gamurar.dev@gmail.com>
2024-04-21 17:01:22 +0300

    showcase Singleton pattern

--- lab1-creational/DatabaseConnection.ts
+++ lab1-creational/DatabaseConnection.ts
@@ -1,18 +1,20 @@
 // src/DatabaseConnection.ts
+import { getRandomInt } from "./utils";
+
 export class DatabaseConnection {
   private static instance: DatabaseConnection;
 
-  private constructor(private connectionString: string) {}
+  private constructor(private connectionId: number) {}
 
   static getInstance(): DatabaseConnection {
       if (!DatabaseConnection.instance) {
           console.log("Creating new database connection instance.");
-          DatabaseConnection.instance = new DatabaseConnection("YourConnectionString");
+          DatabaseConnection.instance = new DatabaseConnection(getRandomInt(1, 100));
       }
       return DatabaseConnection.instance;
   }
 
   query(sql: string): void {
-      console.log(`Querying database with SQL: ${sql}`);
+      console.log(`[DB connection ${this.connectionId}]: ${sql}`);
   }
 }
--- lab1-creational/Order.ts
+++ lab1-creational/Order.ts
@@ -1,8 +1,12 @@
 // src/Order.ts
+import { DatabaseConnection } from "./DatabaseConnection";
 import { Product } from "./Product";
 
 export class Order {
-    constructor(public orderId: number, public products: Product[], public username: string) {}
+    constructor(public orderId: number, public products: Product[], public username: string) {
+        const db = DatabaseConnection.getInstance();
+        db.query(`INSERT INTO orders (order_id, products, username) VALUES (${orderId}, ${JSON.stringify(products)}, ${username})`);
+    }
 
     clone(): Order {
         console.log(`Cloning order ${this.orderId}`);
